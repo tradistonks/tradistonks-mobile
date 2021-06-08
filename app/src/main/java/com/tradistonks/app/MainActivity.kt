ab59584f103a6f8988bec13d7404e0c21e7b69f2
Marion <katyanne_mm@live.fr>
2021-06-08 12:14:50 +0200

    Create menu

--- app/src/main/java/com/tradistonks/app/MainActivity.kt
+++ app/src/main/java/com/tradistonks/app/MainActivity.kt
@@ -1,32 +1,82 @@
 package com.tradistonks.app
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.tradistonks.app.ui.theme.TradistonksAndroidTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DefaultPreview()
+            TradistonksAndroidTheme {
+                AppMainScreen()
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Row(
-        Modifier.padding(5.dp).fillMaxWidth().fillMaxHeight()
-    ) {
-        Column {
-            Text(text = "Hello $name!")
+fun AppMainScreen() {
+    val navController = rememberNavController()
+    Surface(color = MaterialTheme.colors.background) {
+        val drawerState = rememberDrawerState(DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        val openDrawer = {
+            scope.launch {
+                drawerState.open()
+            }
+        }
+        ModalDrawer(
+            drawerState = drawerState,
+            gesturesEnabled = drawerState.isOpen,
+            drawerContent = {
+                Drawer(
+                    onDestinationClicked = { route ->
+                        scope.launch {
+                            drawerState.close()
+                        }
+                        navController.navigate(route) {
+                            popUpTo(navController.graph.startDestinationId)
+                            launchSingleTop = true
+                        }
+                    }
+                )
+            }
+        ) {
+            NavHost(
+                navController = navController,
+                startDestination = DrawerScreens.Home.route
+            ) {
+                composable(DrawerScreens.Home.route) {
+                    Home(
+                        openDrawer = {
+                            openDrawer()
+                        }
+                    )
+                }
+                composable(DrawerScreens.Account.route) {
+                    Account(
+                        openDrawer = {
+                            openDrawer()
+                        }
+                    )
+                }
+                composable(DrawerScreens.Help.route) {
+                    Help(
+                        navController = navController
+                    )
+                }
+            }
         }
     }
 }
@@ -35,6 +85,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     TradistonksAndroidTheme {
-        Greeting("Android")
+        AppMainScreen()
     }
 }
