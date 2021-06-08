ae760c0341f22c7e1df0a2c92b0807c0c1469276
Marion <katyanne_mm@live.fr>
2021-06-08 12:38:39 +0200

    Refacto of the menu

--- app/src/main/java/com/tradistonks/app/MainActivity.kt
+++ app/src/main/java/com/tradistonks/app/MainActivity.kt
@@ -4,94 +4,16 @@ package com.tradistonks.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.tradistonks.app.ui.theme.TradistonksAndroidTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TradistonksAndroidTheme {
-                AppMainScreen()
-            }
-        }
-    }
-}
-
-@Composable
-fun AppMainScreen() {
-    val navController = rememberNavController()
-    Surface(color = MaterialTheme.colors.background) {
-        val drawerState = rememberDrawerState(DrawerValue.Closed)
-        val scope = rememberCoroutineScope()
-        val openDrawer = {
-            scope.launch {
-                drawerState.open()
-            }
-        }
-        ModalDrawer(
-            drawerState = drawerState,
-            gesturesEnabled = drawerState.isOpen,
-            drawerContent = {
-                Drawer(
-                    onDestinationClicked = { route ->
-                        scope.launch {
-                            drawerState.close()
-                        }
-                        navController.navigate(route) {
-                            popUpTo(navController.graph.startDestinationId)
-                            launchSingleTop = true
-                        }
-                    }
-                )
-            }
-        ) {
-            NavHost(
-                navController = navController,
-                startDestination = DrawerScreens.Home.route
-            ) {
-                composable(DrawerScreens.Home.route) {
-                    Home(
-                        openDrawer = {
-                            openDrawer()
-                        }
-                    )
-                }
-                composable(DrawerScreens.Account.route) {
-                    Account(
-                        openDrawer = {
-                            openDrawer()
-                        }
-                    )
-                }
-                composable(DrawerScreens.History.route) {
-                    History(
-                        openDrawer = {
-                            openDrawer()
-                        }
-                    )
-                }
-                composable(DrawerScreens.Strategies.route) {
-                    Strategies(
-                        openDrawer = {
-                            openDrawer()
-                        }
-                    )
-                }
-                composable(DrawerScreens.Dashboard.route) {
-                    Dashboard(
-                        openDrawer = {
-                            openDrawer()
-                        }
-                    )
-                }
+                MainMenu()
             }
         }
     }
@@ -101,6 +23,6 @@ fun AppMainScreen() {
 @Composable
 fun DefaultPreview() {
     TradistonksAndroidTheme {
-        AppMainScreen()
+        MainMenu()
     }
 }
