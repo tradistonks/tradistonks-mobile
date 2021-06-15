74079eccdc773d7f26e32ec65e801ff85834484b
Marion MOTA <katyanne_mm@live.fr>
2021-06-15 09:26:48 +0200

    Adding Strategies view

---
+++ app/src/main/java/com/tradistonks/app/components/Strategies.kt
@@ -0,0 +1,8 @@
+package com.tradistonks.app.components
+
+import java.util.*
+
+data class Strategy(val _id: String, val user: String, val name: String,
+                  val language: String, val updated_date: Date, val created_date: Date){
+}
+
--- app/src/main/java/com/tradistonks/app/pages/Strategies.kt
+++ app/src/main/java/com/tradistonks/app/pages/Strategies.kt
@@ -1,10 +1,8 @@
 package com.tradistonks.app.pages
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import android.graphics.Paint
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -12,6 +10,15 @@ import androidx.compose.ui.res.stringResource
 import com.tradistonks.app.R
 import com.tradistonks.app.components.Page
 import com.tradistonks.app.ui.theme.textColor
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.ui.unit.dp
+import com.tradistonks.app.STRATEGIES_LIST
+import com.tradistonks.app.components.Strategy
+import com.tradistonks.app.ui.theme.colorFont
 
 @Composable
 fun Strategies(openDrawer: () -> Unit) {
@@ -24,9 +31,57 @@ fun pageStrategies(){
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Strategies.",
+        Text(text = stringResource(id = R.string.title_page_strategies),
             style = MaterialTheme.typography.h1,
             color = textColor
         )
+        LiveDataComponentList(STRATEGIES_LIST)
+    }
+}
+
+@Composable
+fun LiveDataComponentList(strategyList: List<Strategy>) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(items = strategyList, itemContent = { strategy ->
+            Card(
+                shape = RoundedCornerShape(4.dp),
+                backgroundColor = colorFont,
+                modifier = Modifier
+                    .fillParentMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Row(modifier = Modifier) {
+                    Box{
+                        Column(modifier = Modifier){
+                            Text(
+                                text = strategy.name,
+                                style = MaterialTheme.typography.h2,
+                                color = textColor
+                            )
+                            Text(
+                                text = "language: ${strategy.language}",
+                                style = MaterialTheme.typography.body1,
+                                color = textColor
+                            )
+                            Text(
+                                text = "last update: ${strategy.updated_date}",
+                                style = MaterialTheme.typography.body1,
+                                color = textColor
+                            )
+                        }
+
+                    }
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.End
+                    ) {
+                        Icon(
+                            Icons.Outlined.Send,
+                            contentDescription = stringResource(id = R.string.run),
+                        )
+                    }
+                }
+            }
+        })
     }
 }
