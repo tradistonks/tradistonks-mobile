f3b74a16596256267c02702010fd414889b2292d
Marion MOTA <katyanne_mm@live.fr>
2021-06-16 08:03:48 +0200

    Adding History view

--- app/src/main/java/com/tradistonks/app/MainActivity.kt
+++ app/src/main/java/com/tradistonks/app/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.tradistonks.app.components.Order
 import com.tradistonks.app.components.Strategy
 import com.tradistonks.app.components.User
 import com.tradistonks.app.ui.theme.TradistonksAndroidTheme
@@ -17,6 +18,16 @@ var STRATEGIES_LIST: List<Strategy> = listOf<Strategy>(
     Strategy("2", "Test", "Sell", "Java", Date(), Date()),
     Strategy("3", "Test", "Buy Actions", "Kotlin", Date(), Date()),
     Strategy("4", "Test", "Buy", "Rust", Date(), Date())
+)
+
+var ORDER_LIST: List<Order> = listOf<Order>(
+    Order(type =  "buy", symbol = "AAPL",price = 129.64f,quantity = 2, Date()),
+    Order(type =  "buy", symbol = "AAPL",price = 129.64f,quantity = 2, Date()),
+    Order(type =  "buy", symbol = "AAPL",price = 129.64f,quantity = 2, Date()),
+    Order(type =  "buy", symbol = "AAPL",price = 129.64f,quantity = 2, Date()),
+    Order(type =  "buy", symbol = "AAPL",price = 129.64f,quantity = 2, Date()),
+    Order(type =  "buy", symbol = "AAPL",price = 129.64f,quantity = 2, Date()),
+    Order(type =  "buy", symbol = "AAPL",price = 129.64f,quantity = 2, Date())
 )
 
 
--- app/src/main/java/com/tradistonks/app/pages/History.kt
+++ app/src/main/java/com/tradistonks/app/pages/History.kt
@@ -1,16 +1,24 @@
 package com.tradistonks.app.pages
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.tradistonks.app.ORDER_LIST
 import com.tradistonks.app.R
+import com.tradistonks.app.components.Order
 import com.tradistonks.app.components.Page
+import com.tradistonks.app.ui.theme.colorFont
+import com.tradistonks.app.ui.theme.colorYellow
 import com.tradistonks.app.ui.theme.textColor
 
 
@@ -25,10 +33,75 @@ fun pageHistory(){
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            text = "History.",
+        Text(text = stringResource(id = R.string.title_page_history),
             style = MaterialTheme.typography.h1,
             color = textColor
         )
+        LiveDataComponentOrderList(ORDER_LIST)
+    }
+}
+
+@Composable
+fun LiveDataComponentOrderList(orderList: List<Order>) {
+    LazyColumn(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(10.dp),) {
+        items(items = orderList, itemContent = { order ->
+            Card(
+                border = BorderStroke(1.dp, colorYellow),
+                shape = RoundedCornerShape(4.dp),
+                backgroundColor = colorFont,
+                modifier = Modifier
+                    .fillParentMaxWidth()
+            ) {
+                Column(modifier = Modifier.padding(16.dp)){
+                    Column(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                        Text(
+                            text = order.type.uppercase(),
+                            style = MaterialTheme.typography.h2,
+                            color = colorYellow
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Row() {
+                        Box{
+                            Column(modifier = Modifier){
+                                Text(
+                                    text = "Symbol: ${order.symbol}",
+                                    style = MaterialTheme.typography.body1,
+                                    color = textColor
+                                )
+                                Text(
+                                    text = "Date: ${order.created_date}",
+                                    style = MaterialTheme.typography.body1,
+                                    color = textColor
+                                )
+                            }
+                        }
+                        Column(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            Text(
+                                text = "Price : ${order.price}",
+                                style = MaterialTheme.typography.body1,
+                                color = textColor
+                            )
+                            Text(
+                                text = "Quantity : ${order.quantity}",
+                                style = MaterialTheme.typography.body1,
+                                color = textColor
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Column(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                        Text(
+                            text = "Total : ${order.quantity * order.price}",
+                            style = MaterialTheme.typography.h2,
+                            color = colorYellow
+                        )
+                    }
+                }
+            }
+        })
     }
 }
