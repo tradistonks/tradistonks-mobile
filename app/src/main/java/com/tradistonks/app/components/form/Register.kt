d5644c3b83a8f3cfeddd8eaa85bbd48ea9ea5d37
Marion MOTA <katyanne_mm@live.fr>
2021-06-23 01:22:58 +0200

    Update User variables

--- app/src/main/java/com/tradistonks/app/components/form/Register.kt
+++ app/src/main/java/com/tradistonks/app/components/form/Register.kt
@@ -36,7 +36,7 @@ fun RegisterContent() {
         val confirmationPasswordFocusRequest = remember { FocusRequester() }
         val emailState = remember { EmailField() }
         val fieldState = remember { Field() }
-        Field(fieldState, onImeAction = { passwordFocusRequest.requestFocus() }, text = GLOBAL_USER.username)
+        Field(fieldState, onImeAction = { passwordFocusRequest.requestFocus() }, text = GLOBAL_USER!!.username)
         Spacer(modifier = Modifier.height(16.dp))
         Email(emailState, onImeAction = { passwordFocusRequest.requestFocus() })
 
