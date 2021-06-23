e3dc16cf8291c2af8b9e6be8bb549a47903e7a38
Marion MOTA <katyanne_mm@live.fr>
2021-06-23 22:46:38 +0200

    Display charts

---
+++ app/src/main/java/com/tradistonks/app/components/charts/ScreenContainer.kt
@@ -0,0 +1,26 @@
+package com.tradistonks.app.components.charts
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ScreenContainer(
+  content: LazyListScope.() -> Unit,
+) {
+  LazyColumn(
+    modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.spacedBy(24.dp),
+    contentPadding = PaddingValues(
+      top = 24.dp,
+      bottom = 24.dp,
+    ),
+  ) {
+    content()
+  }
+}
